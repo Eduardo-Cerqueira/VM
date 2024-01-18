0d3f292f9d54a8953199fd623aeb320368d9018c
Eduardo-Cerqueira <eduardoccerqueira778@gmail.com>
2024-01-18 13:15:07 +0100

    :hammer: Fix curl save file in tmp folder

--- scripts/database.sh
+++ scripts/database.sh
@@ -3,14 +3,14 @@
 
 # Install dependencies
 apt-get update
-apt-get install postgresql curl acl
+apt-get install postgresql curl acl -y
 
 # Enable Postgresql service
 systemctl enable postgresql
 systemctl start postgresql
 
 # Fetch Demeter Postgresql init script
-su postgres -c "curl -O https://raw.githubusercontent.com/Eduardo-Cerqueira/demeter/staging/init.sql"
+curl "https://raw.githubusercontent.com/Eduardo-Cerqueira/demeter/staging/init.sql" > /tmp/init.sql
 
 # Execute the init script
-su postgres -c "psql -U postgres -c '\i ./init.sql'"
+su postgres -c "psql -U postgres -c '\i /tmp/init.sql'"
