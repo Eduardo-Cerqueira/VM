2fca78a83d1ecc318fa45e62c1f625237c60929b
Eduardo-Cerqueira <eduardoccerqueira778@gmail.com>
2024-01-18 11:30:06 +0100

    :hammer: Add curl inside postgres user

--- scripts/database.sh
+++ scripts/database.sh
@@ -10,7 +10,7 @@ systemctl enable postgresql
 systemctl start postgresql
 
 # Fetch Demeter Postgresql init script
-curl -O https://raw.githubusercontent.com/Eduardo-Cerqueira/demeter/staging/init.sql
+su postgres -c "curl -O https://raw.githubusercontent.com/Eduardo-Cerqueira/demeter/staging/init.sql"
 
 # Execute the init script
 su postgres -c "psql -U postgres -c '\i ./init.sql'"
