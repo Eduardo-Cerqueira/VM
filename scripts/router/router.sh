d5a9f7daecb1eb8d286a0936014a10c80c114c84
Eduardo-Cerqueira <eduardoccerqueira778@gmail.com>
2024-01-19 10:28:43 +0100

    :hammer: Add router setup script

---
+++ scripts/router/router.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+## ⚠️ Script should be launched as "root" using su (sudo not required)
+
+# Copy interfaces example
+cp interfaces.example > /etc/network/interfaces
+
+# Set automatic iptables ipv4 save at install (remove interaction)
+debconf-set-selections <<EOF
+iptables-persistent iptables-persistent/autosave_v4 boolean true
+iptables-persistent iptables-persistent/autosave_v6 boolean true
+EOF
+
+# Install dependencies (iptables if not installed)
+apt-get -y install iptables iptables-persistent
+
+# Activate port fowarding by adding line in /etc/sysctl.conf
+echo 'net.ipv4.ip_forward=1' >> /etc/sysctl.conf
+
+# Persist changes
+sysctl -p
+
+iptables -t nat -A POSTROUTING -o enp0s3 -j MASQUERADE
