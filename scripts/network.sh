764b0e1dba7baf133f40d6f74ec4a000d803a017
Eduardo-Cerqueira <eduardoccerqueira778@gmail.com>
2024-01-19 10:55:57 +0100

    :hammer: Add network restart

--- scripts/network.sh
+++ scripts/network.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 ## ⚠️ Script should be launched as "root" using su (sudo not required)
+## Don't forget to edit interfaces.example before
 
 # Copy interfaces example
 cp interfaces.example /etc/network/interfaces
 
 # Copy resolv.conf example
 cp resolv.conf.example /etc/resolv.conf
+
+# Restart network
+systemctl restart networking
--- scripts/router/router.sh
+++ scripts/router/router.sh
@@ -20,3 +20,6 @@ echo 'net.ipv4.ip_forward=1' >> /etc/sysctl.conf
 sysctl -p
 
 iptables -t nat -A POSTROUTING -o enp0s3 -j MASQUERADE
+
+# Restart network
+systemctl restart networking
