1b15967bb1ceee0edb98fd0bd740aa27fdabb07a
Val <pro.mvaldo@gmail.com>
2024-01-19 09:33:19 +0100

    🔨Update cloned repo

--- scripts/frontend.bash
+++ scripts/frontend.bash
@@ -6,8 +6,8 @@ source ~/.bashrc
 nvm install node
 npm i -g pm2
 
-# Clone Vitesse template
-git clone https://github.com/antfu/vitesse
+# Clone Demeter Frontend 
+git clone https://github.com/ValzM/VM-Demeter-Frontend.git
 
 # Install app dependencies
 cd vitesse
@@ -17,7 +17,7 @@ npm install
 npm run build
 
 # Create a process for the app
-pm2 serve dist 3000 --name "vitesse-build"
+pm2 serve dist 3000 --name "demeter-build"
 pm2 save
 
 # Start process at launch
