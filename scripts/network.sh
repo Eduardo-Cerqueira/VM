5bf04abc01665dd878476a0a2c5cbab3112475f5
Eduardo-Cerqueira <eduardoccerqueira778@gmail.com>
2024-01-19 10:33:56 +0100

    :hammer: Add global network setup script

---
+++ scripts/network.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+## ⚠️ Script should be launched as "root" using su (sudo not required)
+
+# Copy interfaces example
+cp interfaces.example > /etc/network/interfaces
+
+# Copy resolv.conf example
+cp resolv.conf.example > /etc/resolv.conf
