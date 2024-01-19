781aa9cfe18414c33e9fa848776b62381c217e78
JeffY1710 <jeff.yabas@gmail.com>
2024-01-19 09:24:56 +0100

    add .env creation

--- scripts/backend/launch.sh
+++ scripts/backend/launch.sh
@@ -1,8 +1,18 @@
+
+
 # Get the project back-end
 
 git clone https://github.com/Eduardo-Cerqueira/demeter.git
 
 cd demeter
+
+touch .env
+
+echo 'DATABASE_HOST=[X.X.X.X]' >> .env
+echo 'DATABASE_PORT=5432' >> .env
+echo 'DATABASE_NAME=demeter' >> .env
+echo 'DATABASE_USER=app' >> .env
+echo 'DATABASE_PASSWORD=app' >> .env
 
 git checkout staging
 
