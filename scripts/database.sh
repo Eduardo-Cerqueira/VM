a40ae820ae53226782698a0ee21a3d234a7639dd
Eduardo-Cerqueira <eduardoccerqueira778@gmail.com>
2024-01-19 14:24:21 +0100

    :hammer: Add postgresql remote connection

--- scripts/database.sh
+++ scripts/database.sh
@@ -15,11 +15,20 @@ curl "https://raw.githubusercontent.com/Eduardo-Cerqueira/demeter/staging/init.s
 # Execute the init script
 su postgres -c "psql -U postgres -c '\i /tmp/init.sql'"
 
+# Get postgresql version
+postgres_version="$(su postgres -c 'ls ~/')"
+
+echo listen_addresses = '192.168.56.102' >> "/etc/postgresql/$postgres_version/main/postgresql.conf"
+
+echo host    demeter     	app     192.168.56.102/24       md5 >> "/etc/postgresql/$postgres_version/main/pg_hba.conf"
+
+systemctl restart postgresql 
+
 # Open postgresql port for API ip
-iptables -A INPUT -p tcp --dport 5432 -s 192.168.56.102 -j ACCEPT
+/sbin/iptables -A INPUT -p tcp --dport 5432 -s 192.168.56.102 -j ACCEPT
 
 # Save iptables config
-iptables-save > /etc/iptables/rules.v4
+/sbin/iptables-save > /etc/iptables/rules.v4
 
 # Restart network
 systemctl restart networking
